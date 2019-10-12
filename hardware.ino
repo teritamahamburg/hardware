34ff5988c4e8ed0cec54f803aefd48c0feb841b9
salepon <>
2019-10-12 14:24:30 +0900

    :construction: leds

--- hardware.ino
+++ hardware.ino
@@ -10,11 +10,18 @@ Adafruit_Thermal printer(&printSerial);
 #include <Wire.h>
 #include <S11059.h>
 
+#define SW 4
+#define LED 5
+#define BUZZ 3
+
 int color_R = 0, color_G = 0, color_B = 0;
 
 S11059 colorSensor;
 
 void setup() {
+  pinMode(SW, INPUT_PULLUP);
+  pinMode(LED, OUTPUT);
+
   printSerial.begin(9600);
   printer.begin();
   Serial.begin(9600);
@@ -37,18 +44,24 @@ void setup() {
 }
 
 void loop() {
+  while (digitalRead(SW));
+  tone(BUZZ, 440);
+  digitalWrite(LED, 1);
   colorSensor.delay();
+  colorSensor.update();
+
   color_R = colorSensor.getRed();
   color_G = colorSensor.getGreen();
   color_B = colorSensor.getBlue();
 
-  colorSensor.update();
   printer.print(F("R: "));
   printer.print(color_R);
-  printer.print(F(" G: "));
+  printer.print(F("   G: "));
   printer.print(color_G);
-  printer.print(F(" B: "));
+  printer.print(F("   B: "));
   printer.print(color_B);
   printer.println();
-  delay(2000);
+  noTone(BUZZ);
+  digitalWrite(LED, 0);
+  delay(200);
 }
